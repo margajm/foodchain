d9d03a691d8cde55e2d4a6ff9657aa0dc6445d91
Julio M Cruz <julio.cruz@eb-ms.net>
2024-06-19 20:12:15 -0400

    Update initial page

--- FoodChain/app/page.tsx
+++ FoodChain/app/page.tsx
@@ -1,9 +1,41 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [showLogo, setShowLogo] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLogo(true);
+      console.log("Show Logo");
+    }, 6000);
+    return () => clearTimeout(timer); // Cleanup timer on component unmount
+  }, []);  
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>FoodChain</h1>
-    </main>
+    <div className="flex items-center justify-center h-screen bg-gradient-to-b from-[#c5a30a] via-[#c5a30a] to-[#f2c810] p-2">
+
+      {!showLogo && (
+        <Image src="/foochain-video01.gif" alt="Loading..." 
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: 'auto', height: '90%' }}
+        className="rounded-xl shadow-xl m-2 ${showLogo ? 'animate-vanish' : ''}"/>
+      )}
+
+      {showLogo && (
+        <div className="absolute flex flex-col items-center justify-center animate-slide-up">
+          {/* <img src="/path-to-your-logo.png" alt="Logo" className="mb-4 w-32 h-32" /> */}
+          <button className="bg-blue-500 text-white px-4 py-2 rounded">
+            Login
+          </button>
+        </div>
+      )}
+
+    </div>
   );
 }
